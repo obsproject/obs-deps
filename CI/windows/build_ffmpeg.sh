7e2acecef5a4d2fe59bedd09eee169d5529d6739
Ryan Foster <RytoEX@gmail.com>
2022-01-27 10:20:57 -0500

    windows: Update libaom and SVT-AV1
    Update the libaom and SVT-AV1 versions and also apply several
    cherry-picked commits to FFmpeg for SVT-AV1.

--- CI/windows/build_ffmpeg.sh
+++ CI/windows/build_ffmpeg.sh
@@ -34,6 +34,13 @@ _patch_product() {
     # http://ffmpeg.org/pipermail/ffmpeg-devel/2021-November/287914.html
     apply_patch "${CHECKOUT_DIR}/CI/patches/FFmpeg-4.4.1-libaomenc.patch" "AEDBA40CEA296D73CBF8BFC6365C0D93237F9177986B96B07C1D2C4C5CFB896C"
     apply_patch "${CHECKOUT_DIR}/CI/patches/FFmpeg-4.4.1-librist.patch" "1B95F21375421830263A73C74B80852E60EFE10991513CFCC8FB7CBE066887F5"
+
+    # Apply patches for libavcodec/libsvtav1
+    apply_patch "https://github.com/obsproject/FFmpeg/commit/9ee65983b32b3aa637a839f5171aa16d7bc3650d.patch?full_index=1" "a7b0850f6ab1e688a02ba98f05b2a60d6fc1cb306ca825a3bd4e7eacb2fc0a75"
+    apply_patch "https://github.com/obsproject/FFmpeg/commit/3558b7c140f86551cd65e7e7aa9815cc2db6e16b.patch?full_index=1" "865bbc3dd389569786a6f6972faee7d3e36a7f0d724226c286dd2dfa8ac4efdf"
+    apply_patch "https://github.com/obsproject/FFmpeg/commit/8451b7c1d4ade3477b9446b8cd5bfd6ddbf71e83.patch?full_index=1" "5c41f4702927b0dc35fae9d22f32f6d2ac54f69ca7042e375a38ffdd17fff3af"
+    apply_patch "https://github.com/obsproject/FFmpeg/commit/2927d888cbfda5d19b3147eb5b3a6f423b23cc33.patch?full_index=1" "5d00f30410a3ceb8c47bcd14935151ead13ed834d87e570771836b1e3e7b768a"
+
     git add .
     git commit -m "OBS patches for flvenc, hlsenc, aomenc & librist"
 }
