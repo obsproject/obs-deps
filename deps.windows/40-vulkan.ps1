fa2a2556a1d975987a2541474cb8ca9d75e69231
Ryan Foster <ryan@obsproject.com>
2022-07-29 18:56:06 -0400

    deps.windows: Update VulkanSDK to 1.3.216.0
    Continue using our own hosted Vulkan components.
    
    The Vulkan SDK installer no longer includes the 32-bit components in it.
    They are now an optional component downloaded during installation. Use
    our own hosted copy of the components instead.
    
    Additionally, this is effectively a double revert.
    
    Revert "Revert "deps.windows: Update VulkanSDK to 1.3.216.0""
    
    This reverts commit d876ba352ebc10f1af0c2eb1a22a6007975f0219.

--- deps.windows/40-vulkan.ps1
+++ deps.windows/40-vulkan.ps1
@@ -1,8 +1,8 @@
 param(
     [string] $Name = 'vulkansdk',
-    [string] $Version = '1.2.131.2',
-    [string] $Uri = 'https://cdn-fastly.obsproject.com/downloads/VulkanSDK-1.2.131.2-Installer-Components.7z',
-    [string] $Hash = "${PSScriptRoot}/checksums/VulkanSDK-1.2.131.2-Installer-Components.7z.sha256"
+    [string] $Version = '1.3.216.0',
+    [string] $Uri = 'https://cdn-fastly.obsproject.com/downloads/VulkanSDK-1.3.216.0-Installer-Components.7z',
+    [string] $Hash = "${PSScriptRoot}/checksums/VulkanSDK-1.3.216.0-Installer-Components.7z.sha256"
 )
 
 function Setup {
