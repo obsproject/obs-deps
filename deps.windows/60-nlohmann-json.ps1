6d1ada9aa0ea4418561a3478851153f17acf96bd
tytan652 <tytan652@tytanium.xyz>
2022-11-05 16:56:03 +0100

    deps.windows: Add nlohmann JSON to Windows deps

---
+++ deps.windows/60-nlohmann-json.ps1
@@ -0,0 +1,63 @@
+param(
+    [string] $Name = 'nlohmann-json',
+    [string] $Version = '3.11.2',
+    [string] $Uri = 'https://github.com/nlohmann/json.git',
+    [string] $Hash = 'bc889afb4c5bf1c0d8ee29ef35eaaf4c8bef8a5d'
+)
+
+function Setup {
+    Setup-Dependency -Uri $Uri -Hash $Hash -DestinationPath $Path
+}
+
+function Clean {
+    Set-Location $Path
+
+    if ( Test-Path "build_${Target}" ) {
+        Log-Information "Clean build directory (${Target})"
+        Remove-Item -Path "build_${Target}" -Recurse -Force
+    }
+}
+
+function Configure {
+    Log-Information "Configure (${Target})"
+    Set-Location $Path
+
+    $Options = @(
+        $CmakeOptions
+        '-DJSON_BuildTests=OFF'
+    )
+
+    Invoke-External cmake -S . -B "build_${Target}" @Options
+}
+
+function Build {
+    Log-Information "Build (${Target})"
+    Set-Location $Path
+
+    $Options = @(
+        '--build', "build_${Target}"
+        '--config', $Configuration
+    )
+
+    if ( $VerbosePreference -eq 'Continue' ) {
+        $Options += '--verbose'
+    }
+
+    Invoke-External cmake @Options
+}
+
+function Install {
+    Log-Information "Install (${Target})"
+    Set-Location $Path
+
+    $Options = @(
+        '--install', "build_${Target}"
+        '--config', $Configuration
+    )
+
+    if ( $Configuration -match "(Release|MinSizeRel)" ) {
+        $Options += '--strip'
+    }
+
+    Invoke-External cmake @Options
+}
