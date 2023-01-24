db1ad0dc6ad3524d055875b2c9211ac624ff6520
tytan652 <tytan652@tytanium.xyz>
2023-01-24 16:39:20 +0100

    deps.ffmpeg: Update AMF to 1.4.29

--- deps.ffmpeg/80-amf.zsh
+++ deps.ffmpeg/80-amf.zsh
@@ -2,9 +2,9 @@ autoload -Uz log_debug log_error log_info log_status log_output dep_checkout
 
 ## Dependency Information
 local name='amf'
-local version='1.4.26'
+local version='1.4.29'
 local url='https://github.com/GPUOpen-LibrariesAndSDKs/AMF.git'
-local hash='5c78f194a396938ff0e72128712252151238e372'
+local hash='bc64bf6e8bfca3083755b7cbec3c23d5546fb04a'
 
 ## Dependency Overrides
 local targets=('windows-x*')
