841e3e28e8dbecf32165f327d2082b13d9c6ce30
PatTheMav <PatTheMav@users.noreply.github.com>
2023-05-05 01:00:43 +0200

    Update main build script to enable packaging of dSYMs

--- build-deps.zsh
+++ build-deps.zsh
@@ -85,9 +85,11 @@ package() {
   if [[ ${PACKAGE_NAME} != 'qt'* ]] {
     log_status "Cleanup unnecessary files"
 
-    rm -rf lib/^(*.dylib|libajantv*|*.a|*.so*|*.lib|*.framework|*.dSYM|cmake)(N)
-    rm -rf lib/(libpcre*|libpng*|libz.*)(N)
-    rm -rf bin/^(*.exe|*.dll|*.pdb|swig)(N)
+    rm -rf -- lib/^(*.dylib|*.a|*.so*|*.lib|*.framework|*.dSYM|cmake)(N)
+    rm -rf -- lib/(libpcre*|libpng*|libfreetype.a)(N)
+    rm -rf -- bin/^(*.exe|*.dll|*.pdb|swig)(N)
+
+    if [[ ${PACKAGE_NAME} == ffmpeg ]] rm -rf -- lib/*.a(N)
 
     if [[ -f bin/swig ]] {
       swig_lib=(share/swig/*(/))
@@ -99,8 +101,17 @@ package() {
     if [[ -d share ]] rm -rf share/^(swig|cmake)(N)
     if [[ -d cmake ]] rm -rf cmake
     if [[ -d man ]] rm -rf man
-
+  } else {
+    rm -rf -- lib/*.(a|la|prl)(N)
   }
+
+  local -a dsym_files=(**/*.dSYM(N))
+  if [[ ${target} == macos* ]] && (( #dsym_files )) {
+    mkdir -p ${target_config[output_dir]}-dSYMs
+    cp -Rfp ${dsym_files} ${target_config[output_dir]}-dSYMs
+    rm -rf -- ${dsym_files}
+  }
+
   mkdir -p share/obs-deps
   echo "${current_date}" >! share/obs-deps/VERSION
 
@@ -110,6 +121,15 @@ package() {
   _tarflags+=(-cJf)
 
   XZ_OPT=-T0 tar ${_tarflags} ${filename} -- *
+
+  if [[ ${target} == macos* ]] && (( #dsym_files )) {
+    dsym_filename="${filename//.tar.xz/-dSYMs.tar.xz}"
+    log_status "Create archive ${dsym_filename}"
+    pushd ${target_config[output_dir]}-dSYMs
+    XZ_OPT=-T0 tar ${_tarflags} ${dsym_filename} -- *
+    mv -- ${dsym_filename} ${PWD:A:h}
+    popd
+  }
 
   mv -- ${filename} ${PWD:A:h}
 }
