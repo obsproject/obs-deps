7587c943634460520340a94237d49d6c4542419b
Ryan Foster <ryan@obsproject.com>
2023-07-18 17:30:53 -0400

    deps.macos: Update Syphon Framework to 5.0 cb3d8ec506

--- deps.macos/90-syphon.zsh
+++ deps.macos/90-syphon.zsh
@@ -4,7 +4,7 @@ autoload -Uz log_debug log_error log_info log_status log_output
 local name='syphon'
 local version='5.0'
 local url='https://github.com/Syphon/Syphon-Framework.git'
-local hash='fc4f4a2a71c0a8c7539a91093ad26c0c237602d6'
+local hash='cb3d8ec506dc6ed0d5d9d099ffc5e120719a1a6a'
 
 ## Build Steps
 setup() {
