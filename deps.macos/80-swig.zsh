621fb1158389ab0a17ac570c38577693dcc0641f
Ryan Foster <ryan@obsproject.com>
2023-07-18 17:28:53 -0400

    deps.macos: Fix SWIG version number
    The version number currently in place was mistakenly copied from a
    previous version of the libpng build script. This change aligns the
    version number with what is used in the SWIG deps.windows build script.

--- deps.macos/80-swig.zsh
+++ deps.macos/80-swig.zsh
@@ -2,7 +2,7 @@ autoload -Uz log_debug log_error log_info log_status log_output
 
 ## Dependency Information
 local name='swig'
-local version='1.6.37'
+local version='4.1.0'
 local url='https://github.com/swig/swig.git'
 local hash="4dd285fad736c014224ef2ad25b85e17f3dce1f9"
 local patches=(
