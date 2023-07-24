9e0ea1fb1bd38a967b32a8db17a21c8e7dc60ec3
tytan652 <tytan652@tytanium.xyz>
2023-07-24 09:59:05 +0200

    deps.windows: Add uthash to Windows deps

---
+++ deps.windows/60-uthash.ps1
@@ -0,0 +1,31 @@
+param(
+    [string] $Name = 'uthash',
+    [string] $Version = '2.3.0',
+    [string] $Uri = 'https://github.com/troydhanson/uthash.git',
+    [string] $Hash = "e493aa90a2833b4655927598f169c31cfcdf7861"
+)
+
+function Setup {
+    Setup-Dependency -Uri $Uri -Hash $Hash -DestinationPath $Path
+}
+
+function Install {
+    Log-Information "Install (${Target})"
+    Set-Location "$Path"
+
+    New-Item -Path "$($ConfigData.OutputPath)/include" -ItemType Directory -Force *> $null
+
+    $Items = @(
+        @{
+            Path = @("src/utarray.h", "src/uthash.h", "src/utlist.h", "src/utringbuffer.h", "src/utstack.h", "src/utstring.h")
+            Destination = "$($ConfigData.OutputPath)/include"
+            ErrorAction = 'SilentlyContinue'
+        }
+    )
+
+    $Items | ForEach-Object {
+        $Item = $_
+        Log-Output ('{0} => {1}' -f ($Item.Path -join ", "), $Item.Destination)
+        Copy-Item @Item
+    }
+}
