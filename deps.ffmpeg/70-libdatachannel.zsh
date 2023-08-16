ccbe6e05a7fa1780cce3925dfd078214ed509975
PatTheMav <PatTheMav@users.noreply.github.com>
2023-08-16 20:28:59 +0200

    deps.ffmpeg: Add missing fixup step for libdatachannel
    Every dynamic library shipped with OBS Studio needs to properly
    stripped, its debug information converted into a dSYM file and also
    its versioned filename removed from the library ID.
    
    Also fixes a build issue with its dependency mbedtls when using
    Release configuration.

--- deps.ffmpeg/70-libdatachannel.zsh
+++ deps.ffmpeg/70-libdatachannel.zsh
@@ -82,5 +82,50 @@ install() {
 }
 
 fixup() {
-}
+  cd ${dir}
 
+  log_info "Fixup (%F{3}${target}%f)"
+
+  local strip_tool
+  local -a strip_files
+
+  case ${target} {
+    macos*)
+      if (( shared_libs )) {
+        local -a dylib_files=(${target_config[output_dir]}/lib/libdatachannel*.dylib(.))
+
+        autoload -Uz fix_rpaths && fix_rpaths ${dylib_files}
+
+        if [[ ${config} == Release ]] dsymutil ${dylib_files}
+
+        strip_tool=strip
+        strip_files=(${dylib_files})
+      } else {
+        rm -rf -- ${target_config[output_dir]}/lib/libdatachannel*.(dylib|dSYM)(N)
+      }
+      ;;
+    linux*)
+      if (( shared_libs )) {
+        strip_tool=strip
+        strip_files=(${target_config[output_dir]}/lib/libdatachannel.so*(.))
+      } else {
+        rm -rf -- ${target_config[output_dir]}/lib/libdatachannel.so*(N)
+      }
+      ;;
+    windows-x*)
+      if (( shared_libs )) {
+        autoload -Uz create_importlibs
+        create_importlibs ${target_config[output_dir]}/bin/libdatachannel*.dll(.)
+
+        strip_tool=${target_config[cross_prefix]}-w64-mingw32-strip
+        strip_files=(${target_config[output_dir]}/bin/libdatachannel*.dll(.))
+      } else {
+        rm -rf -- ${target_config[output_dir]}/bin/libdatachannel*.dll(N)
+      }
+
+      autoload -Uz restore_dlls && restore_dlls
+      ;;
+  }
+
+  if (( #strip_files )) && [[ ${config} == (Release|MinSizeRel) ]] ${strip_tool} -x ${strip_files}
+}
