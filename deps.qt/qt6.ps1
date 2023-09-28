8d31ed344d3b13a86d4526b326f9099dda4c9937
Ryan Foster <ryan@obsproject.com>
2023-09-28 11:54:42 -0400

    deps.qt: Update Qt6 to 6.5.3 for Windows

--- deps.qt/qt6.ps1
+++ deps.qt/qt6.ps1
@@ -1,7 +1,7 @@
 param(
     [string] $Name = 'qt6',
-    [string] $Version = '6.5.2',
-    [string] $Uri = 'https://download.qt.io/official_releases/qt/6.5/6.5.2',
+    [string] $Version = '6.5.3',
+    [string] $Uri = 'https://download.qt.io/official_releases/qt/6.5/6.5.3',
     [string] $Hash = "${PSScriptRoot}/checksums",
     [array] $Targets = @('x64', 'x86')
 )
