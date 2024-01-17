e8a79cdc2b4dac6fb8d5f7a7c97779f08b4faa0b
Ryan Foster <ryan@obsproject.com>
2024-01-17 17:24:06 -0500

    deps.ffmpeg: Update aom to 3.8.0

--- deps.ffmpeg/40-aom.zsh
+++ deps.ffmpeg/40-aom.zsh
@@ -2,9 +2,9 @@ autoload -Uz log_debug log_error log_info log_status log_output
 
 ## Dependency Information
 local name='aom'
-local version='3.6.1'
+local version='3.8.0'
 local url='https://aomedia.googlesource.com/aom.git'
-local hash='7ade96172b95adc91a5d85bf80c90989cd543ee8'
+local hash='b681eac83963950afc7be55df56c22fa5210aaa2'
 local -a patches=(
   "windows ${0:a:h}/patches/libaom/0001-force-threading-shim-usage.patch \
   6fa9ca74001c5fa3a6521a2b4944be2a8b4350d31c0234aede9a7052a8f1890b"
