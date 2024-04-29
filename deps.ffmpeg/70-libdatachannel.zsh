227e9dc874e31f247450a5e0f1dcf5a1af103e48
Sean DuBois <sean@siobud.com>
2024-04-28 20:43:37 -0400

    deps.ffmpeg: Update libdatachannel to 0.21.0
    This updates to libdatachannel 0.21.0

--- deps.ffmpeg/70-libdatachannel.zsh
+++ deps.ffmpeg/70-libdatachannel.zsh
@@ -2,9 +2,9 @@ autoload -Uz log_debug log_error log_info log_status log_output
 
 ## Dependency Information
 local name='libdatachannel'
-local version='v0.20.1'
+local version='v0.21.0'
 local url='https://github.com/paullouisageneau/libdatachannel.git'
-local hash='7841d9f34cf9bd735958ae203a2536c14240c8a5'
+local hash='9d5c46b8f506943727104d766e5dad0693c5a223'
 
 ## Dependency Overrides
 local -i shared_libs=1
